e45b70ea4efeaeecdce1e3cdef0cb8db26807a29
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-04-19 19:44:18 +0000

    for deployement

--- frontend/src/components/Cardscg.js
+++ frontend/src/components/Cardscg.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Cardscg = ({title}) => {
     return (
-        <Link to = '/shop/?cg=Women'>
 
         <div className="cardscg">
             <h1>{title}</h1>
             <button className="ShopNowcg">Shop Now</button>
         </div>
-        </Link>
     )
 }
 
